b060665ead3e5d46bf2976618f980ae9ef41e2a2
hmesfin <hamelmesfin@gmail.com>
2024-02-09 18:32:35 -0600

    Imported opeai api key into util file

--- gojjo_realty/utils/openapi.py
+++ gojjo_realty/utils/openapi.py
@@ -1,7 +1,7 @@
-from django.conf import settings
 from openai import OpenAI
+from config.settings.base import OPENAI_API_KEY
 
-client = OpenAI(api_key=settings.OPENAI_API_KEY)
+client = OpenAI(api_key=OPENAI_API_KEY)
 
 def generate_summary(text):
         response = client.chat.completions.create(
