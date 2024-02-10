ca0143be51f7ad732d41b790d4ae2d5d278d11d4
hmesfin <hamelmesfin@gmail.com>
2024-02-09 18:15:06 -0600

    Updated css and openai

--- gojjo_realty/utils/openapi.py
+++ gojjo_realty/utils/openapi.py
@@ -1,14 +1,16 @@
 from django.conf import settings
-import openai
+from openai import OpenAI
 
-openai.api_key = settings.OPENAI_API_KEY
-
+client = OpenAI(api_key=settings.OPENAI_API_KEY)
 
 def generate_summary(text):
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[{"role": "system", "content": "You are a helpful assistant."},
-                  {"role": "user", "content": f"Summarize this for a TLDR: {text}"}]
-    )
-    summary = response.choices[0].message.content
-    return summary
+    if api_key is None:
+        return "No API key found"
+    else:
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "system", "content": "You are a helpful assistant."},
+                      {"role": "user", "content": f"Summarize this for a TLDR: {text}"}]
+        )
+        summary = response.choices[0].message.content
+        return summary
