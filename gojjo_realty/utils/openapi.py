bb617513bfab6c6309f1f7993a32645aca6f7ca9
hmesfin <hamelmesfin@gmail.com>
2024-02-09 18:37:43 -0600

    Removed openai

--- gojjo_realty/utils/openapi.py
+++ gojjo_realty/utils/openapi.py
@@ -1,15 +1,13 @@
-from openai import OpenAI
-from config.settings.base import OPENAI_API_KEY
+# from openai import OpenAI
+# from config.settings.base import OPENAI_API_KEY
 
-api_key=OPENAI_API_KEY
+# client = OpenAI(api_key=OPENAI_API_KEY)
 
-client = OpenAI(api_key)
-
-def generate_summary(text):
-        response = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[{"role": "system", "content": "You are a helpful assistant."},
-                      {"role": "user", "content": f"Summarize this for a TLDR: {text}"}]
-        )
-        summary = response.choices[0].message.content
-        return summary
+# def generate_summary(text):
+#         response = client.chat.completions.create(
+#             model="gpt-3.5-turbo",
+#             messages=[{"role": "system", "content": "You are a helpful assistant."},
+#                       {"role": "user", "content": f"Summarize this for a TLDR: {text}"}]
+#         )
+#         summary = response.choices[0].message.content
+#         return summary
