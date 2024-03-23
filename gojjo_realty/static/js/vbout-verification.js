9a111b098a759fc5a58a4cd92d61c977f582c54a
hmesfin <hamelmesfin@gmail.com>
2024-03-23 18:35:47 -0500

    Added vbout tracker

---
+++ gojjo_realty/static/js/vbout-verification.js
@@ -0,0 +1,10 @@
+/* embed code starts */
+var _vbset = _vbset || [];
+_vbset.push(['_account', 'VBT-40782-9350']);
+_vbset.push(['_domain', 'https://gojjorealty.com']);
+
+(function() {
+	var vbt = document.createElement('script'); vbt.type = 'text/javascript'; vbt.async = true; 
+	vbt.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'www.vbt.io/tracker?_account='+_vbset[0][1]+'&_domain='+_vbset[1][1];var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(vbt, s);
+})();
+/* embed code ends */
